7b4633684939e3f9209488747cba584a72be9ded
Guilherme Okita <gui_okita@hotmail.com>
2018-05-19 20:03:59 +0000

    Update Entity.h

--- Project1/Entity.h
+++ Project1/Entity.h
@@ -2,6 +2,7 @@
 #include <glm\glm.hpp>
 #include "TexturedModel.h"
 #include "Mesh.h"
+#include "Animator.h"
 
 using namespace glm;
 
@@ -11,26 +12,26 @@ public:
 	Mesh * mesh;
 	vec3 position;
 	vec3 rotation;
+	//Animator anim;
+	std::string currentAnimation;
 	float scale;
-
 	vector<mat4>transforms;
-
 	float time;
+	bool loopAnimation;
 
 	//int textureIndex;
 
-	void Translate(vec3 pos);
-	void Rotate(vec3 rot);
-
-	//float getTextureXOffset();
-	//float getTextureYOffset();
-
-	Mesh* GetMesh();
-	void Update(float dt);
-
-	vector<mat4> GetTransforms();
-	Entity(Mesh* m, glm::vec3 p, glm::vec3 r, float s);
+	Entity(Mesh* m, glm::vec3 p, glm::vec3 r, float s, std::string startAnimation, bool loopAnimation);
 	//Entity(glm::vec3 position, glm::vec3 rotation, float scale, int textureIndex); //varias textura em um obj
 	~Entity();
+
+	void Translate(vec3 pos);
+	void Rotate(vec3 rot);
+	Mesh* GetMesh();
+	
+	void Update(float dt);
+	vector<mat4> GetTransforms();
+
+	void ChangeAnimation(std::string newAnimation, bool loops);
 };
 
