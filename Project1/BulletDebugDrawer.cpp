fd520588df8271d0ac01ffe5e0d737a23938cfe4
Thiago Dantas <giraya.thiago@gmail.com>
2018-06-28 11:30:20 -0300

    debugDrawer, falta desenhar

--- Project1/BulletDebugDrawer.cpp
+++ Project1/BulletDebugDrawer.cpp
@@ -1,23 +1,77 @@
 #include "BulletDebugDrawer.h"
 
-void BulletDebugDrawer::drawLine(const btVector3 & from, const btVector3 & to, const btVector3 & color)
+#include <stdio.h> //printf debugging
+
+BulletDebugDrawer::BulletDebugDrawer()
+:m_debugMode(0)
 {
-
-	{
-
-		glBegin(GL_LINES);
-		glColor3f(color.getX(), color.getY(), color.getZ());
-		glVertex3f(from.getX(), from.getY(), from.getZ());
-		glVertex3f(to.getX(), to.getY(), to.getZ());
-		glEnd();
-
-	}
+    
 }
 
-void BulletDebugDrawer::ToggleDebugFlag(int flag) {
+void    BulletDebugDrawer::drawLine(const btVector3& from,const btVector3& to,const btVector3& color)
+{
+    //      if (m_debugMode > 0)
+    {
+        float tmp[ 6 ] = { from.getX(), from.getY(), from.getZ(),
+            to.getX(), to.getY(), to.getZ() };
+        
+        glPushMatrix();
+        {
+            glColor4f(color.getX(), color.getY(), color.getZ(), 1.0f);
+            glVertexPointer( 3,
+                            GL_FLOAT,
+                            0,
+                            &tmp );
+            
+            glPointSize( 5.0f );
+            glDrawArrays( GL_POINTS, 0, 2 );
+            glDrawArrays( GL_LINES, 0, 2 );
+        }
+        glPopMatrix();
+    }
+    // Versão PDF LG
+//    {
+//        // draws a simple line of pixels between points.
+//        // use the GL_LINES primitive to draw lines
+//        glBegin(GL_LINES);
+//        glColor3f(color.getX(), color.getY(), color.getZ());
+//        glVertex3f(from.getX(), from.getY(), from.getZ());
+//        glVertex3f(to.getX(), to.getY(), to.getZ());
+//        glEnd();
+//        Chapter 4
+//        Debug rendering
+//    }
+    
+}
 
-	if (debugMode & flag)
-		debugMode = debugMode & (~flag);
-	else
-		debugMode |= flag;
+void    BulletDebugDrawer::setDebugMode(int debugMode)
+{
+    m_debugMode = debugMode;
+}
+
+void    BulletDebugDrawer::draw3dText(const btVector3& location,const char* textString)
+{
+    //glRasterPos3f(location.x(),  location.y(),  location.z());
+    //BMF_DrawString(BMF_GetFont(BMF_kHelvetica10),textString);
+}
+
+void    BulletDebugDrawer::reportErrorWarning(const char* warningString)
+{
+    printf(warningString);
+}
+
+void    BulletDebugDrawer::drawContactPoint(const btVector3& pointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color)
+{
+    {
+        //btVector3 to=pointOnB+normalOnB*distance;
+        //const btVector3&from = pointOnB;
+        //glColor4f(color.getX(), color.getY(), color.getZ(), 1.0f);
+        
+        //BulletDebugDrawer::drawLine(from, to, color);
+        
+        //glRasterPos3f(from.x(),  from.y(),  from.z());
+        //char buf[12];
+        //sprintf(buf," %d",lifeTime);
+        //BMF_DrawString(BMF_GetFont(BMF_kHelvetica10),buf);
+    }
 }
--- Project1/BulletDebugDrawer.h
+++ Project1/BulletDebugDrawer.h
@@ -8,25 +8,22 @@
 #include <btBulletCollisionCommon.h>
 
 class BulletDebugDrawer : public btIDebugDraw {
-
+    int m_debugMode;
+    
 public:
-
-	BulletDebugDrawer() {};
-	void drawLine(const btVector3& from, const btVector3& to, const btVector3& color) override;
-
-	void setDebugMode(int newDebugMode) override { debugMode = newDebugMode; };
-	//int getDebugMode() override{ return debugMode; } ;
-
-	void ToggleDebugFlag(int newFlag);
-
-	void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color) override {};
-
-	void reportErrorWarning(const char* warningString) override {};
-
-	void draw3dText(const btVector3& location, const char* textString) override {};
-
-protected:
-
-	int debugMode = 1;
-
+    
+    BulletDebugDrawer();
+    
+    virtual void   drawLine(const btVector3& from,const btVector3& to,const btVector3& color);
+    
+    virtual void   drawContactPoint(const btVector3& PointOnB,const btVector3& normalOnB,btScalar distance,int lifeTime,const btVector3& color);
+    
+    virtual void   reportErrorWarning(const char* warningString);
+    
+    virtual void   draw3dText(const btVector3& location,const char* textString);
+    
+    virtual void   setDebugMode(int debugMode);
+    
+    virtual int      getDebugMode() const { return m_debugMode;}
+    
 };
