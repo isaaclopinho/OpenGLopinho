9b34a2fb72eaca6999f3d8f8938128ed1c0d163b
isaac <isaaclopinho@gmail.com>
2018-07-07 15:39:10 -0300

    giraya

--- Project1/GameSrc/LevelState.h
+++ Project1/GameSrc/LevelState.h
@@ -149,7 +149,7 @@ public:
 		camera.angleAroundTarget = 180;
 		phyWorld.addPhysicsObject(player);
         //(mass, shape, position, rotation, scale, inercia, entity);
-        attackBoxPlayer = new PhysicsObject(0, PhysicsShape::Box, btVector3(0,10,40), btVector3(0,0,0), btVector3(5,2,5), btVector3(), new Entity(Loader::LoadModel("res/Models/cube.obj"), glm::vec3(0, 10, 40), glm::vec3(0, 0, 0), vec3(5,2,5), "", true));
+        attackBoxPlayer = new PhysicsObject(0, PhysicsShape::Box, btVector3(0,5,40), btVector3(0,0,0), btVector3(5,2,5), btVector3(), new Entity(Loader::LoadModel("res/Models/cube.obj"), glm::vec3(0, 5, 40), glm::vec3(0, 0, 0), vec3(5,2,5), "", true));
 
         phyWorld.addPhysicsObject(attackBoxPlayer);
         AddGameObject(attackBoxPlayer);
@@ -189,18 +189,20 @@ public:
 		}
         
 //        glm::vec3 newPos = glm::vec3(player->entity.position.x, player->entity.position.y + 10, player->entity.position.z + 7);
-        glm::vec3 newPos = Maths::bulletToGlm((player->getWorldRotation() * 3) + player->getWorldPosition());
+        glm::vec3 newPos = Maths::bulletToGlm((player->getWorldRotation().normalized() * 3) + player->getWorldPosition());
 //        glm::vec3 newPos = transform1.
         
         glm::vec3 newRot = glm::vec3(player->entity.rotation.x, player->entity.rotation.y, player->entity.rotation.z);
         
 //        std::cout << "newrot: " << newRot.x << " " <<newRot.y << " " << newRot.z << endl;
         
-        attackBoxPlayer->setPosition(Maths::glmToBullet(newPos));
+        attackBoxPlayer->setPosition(Maths::glmToBullet(Maths::bulletToGlm(player->getWorldPosition()) + (player->getForwardVector()*8.0f)));
 //        attackBoxplayer->getWorldRotation().normalized();
-        attackBoxPlayer->SetRotation(Maths::glmToBullet(newRot));
-//        attackBoxPlayer->entity->position = newPos;
-        attackBoxPlayer->entity->rotation = newRot;
+		//attackBoxPlayer->SetRotation(Maths::glmToBullet(newRot));
+		//attackBoxPlayer->SetRotation(player->getWorldRotation());
+        //attackBoxPlayer->entity->position = newPos;
+        //attackBoxPlayer->entity->rotation = newRot;
+		printf("%f, %f, %f\n", player->getPlayerRot().x, player->getPlayerRot().y, player->getPlayerRot().z);
 
 		camera.Update(dt, player->entity.position, player->entity.rotation);
         
