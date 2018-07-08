0a11c8a3f2f0742e7a7d1f008e6e753dce5f7225
Thiago Dantas <giraya.thiago@gmail.com>
2018-07-08 03:59:59 -0300

    rotation inimigo, follow player if nearby

--- Project1/GameSrc/Enemy.cpp
+++ Project1/GameSrc/Enemy.cpp
@@ -1,5 +1,7 @@
 #include "Enemy.h"
 #include "Player.h"
+#include <glm/gtx/compatibility.hpp>
+#include <glm/gtx/vector_angle.hpp>
 
 Enemy::Enemy(float mass, PhysicsShape shape, btVector3 inercia, Entity* e): PhysicsObject(mass, shape, inercia, e), invulneravel(1){
     
@@ -16,6 +18,38 @@ Enemy::~Enemy(){
 void Enemy::Update(float dt){
     PhysicsObject::Update(dt);
     invulneravel.Update(dt);
+    if (PlayerNearby() == true){
+        btVector3 position = Maths::glmToBullet(glm::lerp(Maths::bulletToGlm(getWorldPosition()), Maths::bulletToGlm(Player::getInstance()->getWorldPosition()), dt * 1));
+        vec3 dir = Maths::bulletToGlm(Player::getInstance()->getWorldPosition() - getWorldPosition());
+        
+        
+        
+        float ang = angle( normalize(dir), vec3(0, 0, 1));
+        
+        if(getWorldPosition().getX() >= Player::getInstance()->getWorldPosition().getX())
+            ang = -angle( normalize(dir), vec3(0, 0, 1));
+        
+            entity->rotation = vec3(entity->rotation.x, entity->rotation.y, degrees(ang));
+        
+        btVector3 rotation = (Player::getInstance()->getWorldPosition() - getWorldPosition()).normalized();
+        printf("%f, %f, %f\n", rotation.x(), rotation.y(), rotation.z());
+//        entity->rotation = Maths::bulletToGlm(rotation);
+//        entity->rotation.x -= 90;
+        if (entity->currentAnimation != "Walk"){
+            entity->ChangeAnimation("Walk", true);
+        }
+        
+        this->setPosition(position);
+    }
+    
+}
+
+bool Enemy::PlayerNearby(){
+    btScalar dist = (Player::getInstance()->getWorldPosition() - getWorldPosition()).norm();
+    bool near = (dist <= 100);
+    if (near) printf("player por perto\n");
+    else printf("player nÃo por perto \n");
+    return near;
 }
 
 void Enemy::RecieveDamage(int damage){
--- Project1/GameSrc/Enemy.h
+++ Project1/GameSrc/Enemy.h
@@ -7,6 +7,7 @@ public:
     Enemy(float mass, PhysicsShape shape, btVector3 inercia, Entity* e);
     ~Enemy();
     void RecieveDamage(int damage);
+    bool PlayerNearby();
     Timer invulneravel;
     virtual void Update(float dt);
 private:
